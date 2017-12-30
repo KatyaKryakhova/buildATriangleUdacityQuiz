89c14ac3bdbdd092aba562e8815f184031d401d8
Ekaterina Kryakhova <31491752+KatyaKryakhova@users.noreply.github.com>
2017-12-30 11:48:30 +0300

    Add files via upload

---
+++ triangle.js
@@ -0,0 +1,25 @@
+/*
+ * Programming Quiz: Build A Triangle (5-3)
+ */
+
+// creates a line of * for a given length
+function makeLine(length) {
+    var line = "";
+    for (var j = 1; j <= length; j++) {
+        line += "* ";
+    }
+    return line + "\n";
+}
+
+
+function buildTriangle(num){
+    var triangle = "";
+    for (i=1; i<=num; i++){
+        triangle += makeLine(i);
+    }
+   return  triangle;
+}
+
+
+
+console.log(buildTriangle(10));
